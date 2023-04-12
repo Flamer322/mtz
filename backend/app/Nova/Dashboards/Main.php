b5fba0609677d73bfc0d99481eb7a6ca6618b8f6
flamer322 <b.bar1enev@gmail.com>
2023-04-12 12:23:26 +0300

    Initial commit

---
+++ backend/app/Nova/Dashboards/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Nova\Dashboards;
+
+use Laravel\Nova\Cards\Help;
+use Laravel\Nova\Dashboards\Main as Dashboard;
+
+class Main extends Dashboard
+{
+    public function label()
+    {
+        return 'Панель информации';
+    }
+
+    public function cards()
+    {
+        return [
+            new Help,
+        ];
+    }
+}
---
+++ backend/app/Nova/UserNova.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Nova;
+
+use App\Models\User;
+use Illuminate\Validation\Rules;
+use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class UserNova extends Resource
+{
+    public static $model = User::class;
+
+    public static $title = 'email';
+
+    public static function label()
+    {
+        return 'Пользователи';
+    }
+
+    public static function singularLabel()
+    {
+        return 'Пользователь';
+    }
+
+    public static $search = [
+        'id', 'name', 'email',
+    ];
+
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Gravatar::make()->maxWidth(50),
+
+            Text::make('Name')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make('Email')
+                ->sortable()
+                ->rules('required', 'email', 'max:254')
+                ->creationRules('unique:users,email')
+                ->updateRules('unique:users,email,{{resourceId}}'),
+
+            Password::make('Password')
+                ->onlyOnForms()
+                ->creationRules('required', Rules\Password::defaults())
+                ->updateRules('nullable', Rules\Password::defaults()),
+        ];
+    }
+}
